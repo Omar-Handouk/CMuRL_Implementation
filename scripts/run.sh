571d4338df9f34d29228973095cf73c9225bd649
Omar-Handouk <omar.handouk@yandex.com>
2020-06-21 03:24:24 +0200

    Modified run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,12 +8,19 @@ trap "clear_interface; echo 'Reverting to original config'; exit" SIGHUP SIGINT 
 
 clear_interface
 
+i=1
 IFS=$'\n'
 for line in `cat ../scenarios.txt`
 do
-    rectified=${line#$"echo 'CMurl@dmin213!' | sudo -S "}
-    echo $rectified
-    eval $rectified
-    sleep 60
+    rest=$((i%3))
+    if [ $rest -eq 0 ]; then
+        echo '>>Rest state<<'
+    else
+        rectified=${line#$"echo 'CMurl@dmin213!' | sudo -S "}
+        echo $rectified
+        eval $rectified
+    fi
+    sleep 20
     clear_interface
+    i=$((i+1))
 done
